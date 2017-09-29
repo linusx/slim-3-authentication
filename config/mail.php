94b97f777d390610b654df4e34a5c522d76aaf22
Bill Van Pelt <github@vp2k.com>
2017-09-29 14:15:59 -0400

    Trying to fix error

--- config/mail.php
+++ config/mail.php
@@ -3,8 +3,8 @@ return [
     'mail' => [
         'host' => env('MAIL_HOST'),
         'port' => env('MAIL_PORT'),
-        'username' => env('MAIL_USERNAME'),
-        'password' => env('MAIL_PASSWORD'),
+        'username' => env('MAIL_USERNAME', ''),
+        'password' => env('MAIL_PASSWORD', ''),
         'from.name' => env('MAIL_FROM_NAME', 'noreply'),
         'from' => env('MAIL_FROM', 'noreply@example.com'),
     ]
