9b98fff6912d9bb23b2bd016629513010c7a46da
Bill Van Pelt <github@vp2k.com>
2017-09-29 14:33:53 -0400

    Updating

--- config/mail.php
+++ config/mail.php
@@ -5,7 +5,7 @@ return [
         'port' => env('MAIL_PORT'),
         'username' => env('MAIL_USERNAME', ''),
         'password' => env('MAIL_PASSWORD', ''),
-        'from.name' => env('MAIL_FROM_NAME', 'noreply'),
+        'fromname' => env('MAIL_FROM_NAME', 'noreply'),
         'from' => env('MAIL_FROM', 'noreply@example.com'),
     ]
 ];
